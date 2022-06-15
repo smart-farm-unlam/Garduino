c0919b9a6a2eb8afb572d8ee3d833be274d8a0a2
Facu Mediotte <facumediotte@gmail.com>
2022-06-15 01:02:55 -0300

    example stationMode

---
+++ examples/esp32/wifi-http/stationMode.ino
@@ -0,0 +1,40 @@
+#include <WebServer.h>
+#include <WiFi.h>
+
+const char* ssid = "Movistar14";
+const char* password = "mate2306";
+
+WebServer server(80); //puerto por defecto 80
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Trying to connect to " + String(ssid));
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("WiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    //método para gestionar las consultas o peticiones a la URL
+    server.on("/", responseRequest);
+    
+    server.begin();
+    Serial.println("HTTP Server started");
+    delay(100);
+
+}
+
+void loop() {
+    server.handleClient();
+}
+
+String response = "<h1> Hola desde ESP32 </h1>";
+
+void responseRequest() {
+    server.send(200, "text/html", response);
+}
